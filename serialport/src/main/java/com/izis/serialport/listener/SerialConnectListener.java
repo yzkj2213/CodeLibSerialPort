4a6c447a0c9991e7eef7f2492757107c7bf295b3
lunxinfeng <892452936@qq.com>
2021-11-04 16:21:01 +0800

    0.3.0 JNI模式不在主动重连，在service模式下会切换成pl2303方式

--- serialport/src/main/java/com/izis/serialport/listener/SerialConnectListener.java
+++ serialport/src/main/java/com/izis/serialport/listener/SerialConnectListener.java
@@ -11,9 +11,11 @@ public interface SerialConnectListener {
 
     /**
      * 连接失败，如果设置了自动重连，则在自动重连次数用完后，依然没连上时触发
+     *
      * @param connectNum 连接次数，连接成功时该次数会重置为0
      */
-    void onConnectFail(int connectNum);
+    default void onConnectFail(int connectNum) {
+    }
 
     /**
      * 异常断开，主动断开时不会触发
